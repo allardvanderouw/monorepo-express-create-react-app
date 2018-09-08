41d5839795b71e5d8f67891bd725df1add0eb6ba
Allard van der Ouw <allard.van.der.ouw@oliver-it.nl>
2018-09-08 16:52:23 +0200

    Update port

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const app = express()
 
-// app.get('/', (req, res) => res.send('Hello World!'))
+const PORT = process.env.PORT || 3000
 
 app.use(express.static('./client/build'))
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.listen(PORT, () => console.log('Example app listening on port 3000!'))
