3bf6a239f8168b31339d8b380fcfe6c57e339ce2
Allard van der Ouw <allard.van.der.ouw@oliver-it.nl>
2019-04-22 15:27:47 +0200

    Update dependencies & build

---
+++ workspace-client/build/precache-manifest.d85ad59040bf3ac0582e098b7571779d.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ec44de2b7499f29ccfcf",
+    "url": "/static/js/main.c02290e5.chunk.js"
+  },
+  {
+    "revision": "bdee0862fa06a4c31c2d",
+    "url": "/static/js/2.ede85739.chunk.js"
+  },
+  {
+    "revision": "9b20aecdebada684f32b282f5643ff98",
+    "url": "/index.html"
+  }
+];
